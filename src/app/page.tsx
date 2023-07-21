c93ccadf8dd14d802acfc7058fe55e6ec928954b
Ricy <1932353948@qq.com>
2023-07-21 22:24:40 +0800

    fix: new layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,18 @@ import { WrapperCard } from "@/components/Card";
 
 export default function Home() {
   return (
-    <main className="p-[24px] flex flex-col gap-y-[32px] min-h-screen max-w-[1920px] items-center justify-center">
-      <WrapperCard className="flex flex-row justify-between w-[732px] text-[24px] leading-[32px]">
-        Goerli Network
-        <a
-          href="https://en.wikipedia.org/wiki/Rock_paper_scissors#Additional_weapons"
-          target="_blank"
-          className="ml-[8px]"
-        >
-          Game rules
-        </a>
-      </WrapperCard>
-      <div className="flex flex-row gap-x-[32px]">
+    <main className="p-[24px] flex justify-center items-center min-h-screen max-w-[1920px]">
+      <div className="grid grid-cols-2 gap-[32px]">
+        <WrapperCard className="col-span-2 flex flex-row justify-between w-[732px] h-fit text-[24px] leading-[32px]">
+          Goerli Network
+          <a
+            href="https://en.wikipedia.org/wiki/Rock_paper_scissors#Additional_weapons"
+            target="_blank"
+            className="ml-[8px]"
+          >
+            Game rules
+          </a>
+        </WrapperCard>
         <FirstHandCard />
         <SecondHandCard />
       </div>
