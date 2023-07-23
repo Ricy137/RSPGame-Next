5c9aaeb4fbd603456f23231f105cfbbbcdace743
Ricy <1932353948@qq.com>
2023-07-23 15:12:15 +0800

    fix: text for switching network

--- src/modules/AuthConnect/index.tsx
+++ src/modules/AuthConnect/index.tsx
@@ -22,7 +22,7 @@ const AuthConnect: React.FC<PropsWithChildren> = ({ children, ...props }) => {
   return (
     <Button onClick={handleExecAction} disabled={loading} {...props}>
       {!accounts || (accounts.length === 0 && "Connect Wallet")}
-      {accounts && !networkMatch && "Switch Wallet"}
+      {accounts && !networkMatch && "Switch Network"}
     </Button>
   );
 };
