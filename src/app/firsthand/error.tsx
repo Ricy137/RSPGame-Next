ea4543c60b08edbe617750e3faa5cbbba4abe62a
Ricy <1932353948@qq.com>
2023-07-23 21:43:28 +0800

    refactor: change error prompts

--- src/app/firsthand/error.tsx
+++ src/app/firsthand/error.tsx
@@ -17,6 +17,7 @@ export default function Error({
   return (
     <div className="flex flex-col items-center gap-y-[24px] w-full">
       <h2>Something went wrong!</h2>
+      <h2>Please refresh the page or go back to landing to resume</h2>
     </div>
   );
 }
